fa83c69a34e9dce6b400f82c9f6c98a209857d3b
hans.abdillh05@gmail.com <hans.abdillh05@gmail.com>
2022-07-16 18:43:41 +0800

    create home

--- index.php
+++ index.php
@@ -22,7 +22,51 @@
    <title>Hello, world!</title>
   </head>
   <body>
-	<h1>Hello, world!</h1>
+
+  <!-- Nav Bar -->
+  <nav class="navbar navbar-light bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white" href="#">Sistem Penilaian Mahasiswa</a>
+    </div>
+  </nav>
+
+  <!-- Jumbotron -->
+  <section class="p-4 shadow-4 rounded-3 bg-white text-center px-5 pt-5">
+    <h2>Selamat Datang</h2>
+    <p>Di Sistem Penilaian Mahasiswa Politeknik Hasnur</p>
+    <hr class="my-2" />
+    </button>
+  </section>
+  <!-- Jumbotron -->
+
+  <!-- Menu -->
+  <section class="container md:d-flex justify-content-center gap-3">
+    <!-- Card -->
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">Kelola Data Mahasiswa</h5>
+        <p class="card-text">Data mahasiswa meliputi NIM, nama, program studi, dan nomor handphone mahasiswa</p>
+        <a href="#" class="btn btn-primary">Lanjut</a>
+      </div>
+    </div>
+    <!-- Card -->
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">Kelola Data Nilai</h5>
+        <p class="card-text">Data nilai meliputi nama mahasiswa, mata kuliah, nilai dan grade</p>
+        <a href="#" class="btn btn-primary">Lanjut</a>
+      </div>
+    </div>
+    <!-- Card -->
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Kelola Data Admin</h5>
+        <p class="card-text">Data admin meliputi username, password, dan avatar</p>
+        <a href="#" class="btn btn-primary">Lanjut</a>
+      </div>
+    </div>
+
+  </section>
 
    <!-- Option 1: Bootstrap Bundle with Popper -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
